ddfa6ab6ee4237a54164ab7f5c922de9a2d54c9f
Prashant Pokhriyal <prashant.pokhriyal09@gmail.com>
2019-12-22 11:55:38 -0800

    Implemented Minimax AI with Max Depth of 5

--- algorithm/connect4.py
+++ algorithm/connect4.py
@@ -3,120 +3,12 @@ import random
 import signal
 from random import randint
 
+
 def sigint_handler(signum, frame):
     sys.exit(0)
 
 
-def necessary_move(board: list, board_size: int, cur_player: int) -> int:
-    for col in range(board_size):
-        if board[0][col] != 0:
-            continue
-        row = 1
-        while row < board_size:
-            if row == board_size-1 and board[row][col] == 0:
-                break
-            elif board[row][col] != 0:
-                row -= 1
-                break
-            row += 1
-        board[row][col] = cur_player
-        win = validate_win(cur_player, board, board_size, row, col)
-        board[row][col] = 0
-        if win:
-            return col
-    return -1
-
-
-def minimax(board: list, board_size: int) -> int:
-    return 0
-
-
-def order_cols_by_height(board: list, board_size: int) -> int:
-    col_heights = [0] * board_size
-    for col in range(board_size):
-        height = board_size
-        for row in range(board_size):
-            if board[row][col] != 0:
-                break
-            height -= 1
-        col_heights[col] = height
-    return col_heights
-
-
-def pick_col_by_height(board: list, board_size: int) -> int:
-    ordered_cols = order_cols_by_height(board, board_size)
-    while True:
-        if len(ordered_cols) == 1:
-            return ordered_cols[0]
-        # min_option = ordered_cols.index(min(ordered_cols))
-        # max_option = ordered_cols.index(max(ordered_cols))
-        # selected_col = random.choice([min_option, min_option, max_option])
-        # if selected_col > 0 and selected_col < board_size-1:
-        #     selected_col = randint(selected_col-1, selected_col+1)
-        min_height = min(ordered_cols)
-        max_height = max(ordered_cols)
-        options = [col for col, height in enumerate(ordered_cols) if (height == min_height or height == max_height)]
-        selected_col = random.choice(options)
-        # if selected_col > 0 and selected_col < board_size-1:
-        #     selected_col = randint(selected_col-1, selected_col+1)
-
-        if board[0][selected_col] != 0:
-            ordered_cols.pop(selected_col)
-            continue
-
-        #Verify that the semi-randomized selections won't cause a loss
-        row = 1
-        while row < board_size:
-            if row == board_size-1 and board[row][selected_col] == 0:
-                break
-            elif board[row][selected_col] != 0:
-                row -= 1
-                break
-            row += 1
-        board[row][selected_col] = 2
-        win = False
-        if row != 0:
-            row -= 1
-            board[row][selected_col] = 1
-            win = validate_win(1, board, board_size, row, selected_col)
-            board[row][selected_col] = 0
-            board[row+1][selected_col] = 0
-        else:
-            board[row][selected_col] = 0
-        if win:
-            continue
-        # if board[0][selected_col] != 0:
-        #     continue
-        else:
-            return selected_col
-
-
-def choose_column(board: list, board_size: int) -> int:
-    """
-    Selects column for AI Player.
-
-    First checks for immediate opportunities (wins) or vulnerabilities (losses).
-    If neither of the above are present, uses the minimax algorithm.
-    """
-    # First check for immediate opportunities or vulnerabilities for AI
-    opportunity = necessary_move(board, board_size, 2)
-    if opportunity != -1:
-        return opportunity
-    # Note that opportunities are 1st priority.
-    # This is because you could stop 1 vulnerability but still have more undetected. But if you win first via 'opportunity,' this is irrelevant.
-    vulnerability = necessary_move(board, board_size, 1)
-    if vulnerability != -1:
-        return vulnerability
-
-    # If neither of the above are present, use minimax algorithm
-    # return minimax(board, board_size)
-
-    # Temporary Selection Process; Remove Later
-    return pick_col_by_height(board, board_size)
-
-
-
-def validate_win(player: int, board: list, board_size: int, row: int, col: int) -> bool:
+def validate_win(board: list, board_size: int, row: int, col: int, player: int) -> bool:
     """
     Validate whether the current player has "connected 4" after his or her latest move.
 
@@ -134,7 +26,7 @@ def validate_win(player: int, board: list, board_size: int, row: int, col: int) 
             break
         count += 1
         cur_row -= 1
-    cur_row = row+1
+    cur_row = row + 1
     while cur_row < board_size:
         if board[cur_row][col] != player:
             break
@@ -151,7 +43,7 @@ def validate_win(player: int, board: list, board_size: int, row: int, col: int) 
             break
         count += 1
         cur_col -= 1
-    cur_col = col+1
+    cur_col = col + 1
     while cur_col < board_size:
         if board[row][cur_col] != player:
             break
@@ -170,8 +62,8 @@ def validate_win(player: int, board: list, board_size: int, row: int, col: int) 
         count += 1
         cur_row -= 1
         cur_col -= 1
-    cur_row = row+1
-    cur_col = col+1
+    cur_row = row + 1
+    cur_col = col + 1
     while cur_col < board_size and cur_row < board_size:
         if board[cur_row][cur_col] != player:
             break
@@ -191,8 +83,8 @@ def validate_win(player: int, board: list, board_size: int, row: int, col: int) 
         count += 1
         cur_row -= 1
         cur_col += 1
-    cur_row = row+1
-    cur_col = col-1
+    cur_row = row + 1
+    cur_col = col - 1
     while cur_col >= 0 and cur_row < board_size:
         if board[cur_row][cur_col] != player:
             break
@@ -204,6 +96,110 @@ def validate_win(player: int, board: list, board_size: int, row: int, col: int) 
 
     # If no winning sequence found, return False
     return False
+
+
+def minimax(board: list, board_size: int, row: int, col: int, max_player: int, cur_depth: int) -> int:
+    # Only need to check for AI Win if it's currently Player 1's turn (the AI just completed its move)
+    if not max_player and validate_win(board, board_size, row, col, 2):
+        return 10
+    # Only need to check for Player 1 Win if it's currently AI's turn (Player 1 just completed his or her move)
+    if max_player and validate_win(board, board_size, row, col, 1):
+        return -10
+    # Check for Tie or Max Depth Reached
+    if all(board[0]) or cur_depth == 5:
+        return 0
+    # Maximizing Player: Player 2 (AI)
+    if max_player:
+        best_score = -1 * sys.maxsize
+        for col in range(board_size):
+            if board[0][col] == 0:
+                row = 1
+                while row < board_size:
+                    if row == board_size - 1 and board[row][col] == 0:
+                        break
+                    elif board[row][col] != 0:
+                        row -= 1
+                        break
+                    row += 1
+                board[row][col] = 2
+                best_score = max(best_score, minimax(board, board_size, row, col, not max_player, cur_depth + 1))
+                board[row][col] = 0
+        return best_score
+    # Minimizing Player: Player 1
+    else:
+        best_score = sys.maxsize
+        for col in range(board_size):
+            if board[0][col] == 0:
+                row = 1
+                while row < board_size:
+                    if row == board_size - 1 and board[row][col] == 0:
+                        break
+                    elif board[row][col] != 0:
+                        row -= 1
+                        break
+                    row += 1
+                board[row][col] = 1
+                best_score = min(best_score, minimax(board, board_size, row, col, not max_player, cur_depth + 1))
+                board[row][col] = 0
+        return best_score
+
+
+def necessary_move(board: list, board_size: int, cur_player: int) -> int:
+    for col in range(board_size):
+        if board[0][col] != 0:
+            continue
+        row = 1
+        while row < board_size:
+            if row == board_size - 1 and board[row][col] == 0:
+                break
+            elif board[row][col] != 0:
+                row -= 1
+                break
+            row += 1
+        board[row][col] = cur_player
+        win = validate_win(board, board_size, row, col, cur_player)
+        board[row][col] = 0
+        if win:
+            return col
+    return -1
+
+
+def choose_column(board: list, board_size: int) -> int:
+    """
+    Selects column for AI Player.
+
+    First checks for immediate opportunities (wins) or vulnerabilities (losses).
+    If neither of the above are present, uses the minimax algorithm.
+    """
+    # First check for immediate opportunities or vulnerabilities for AI
+    # Opportunities for an immediate win take priority.
+    opportunity = necessary_move(board, board_size, 2)
+    if opportunity != -1:
+        return opportunity
+    vulnerability = necessary_move(board, board_size, 1)
+    if vulnerability != -1:
+        return vulnerability
+
+    # If no necessary move is present, use minimax algorithm to determine optimal move
+    best_col = -1
+    best_col_score = -1 * sys.maxsize
+    for col in range(board_size):
+        if board[0][col] == 0:
+            row = 1
+            while row < board_size:
+                if row == board_size - 1 and board[row][col] == 0:
+                    break
+                elif board[row][col] != 0:
+                    row -= 1
+                    break
+                row += 1
+            board[row][col] = 2
+            cur_score = minimax(board, board_size, row, col, False, 1)
+            board[row][col] = 0
+            if cur_score > best_col_score:
+                best_col_score = cur_score
+                best_col = col
+    return best_col
 
 
 def play_game(board_size: int, num_players: int) -> int:
@@ -245,7 +241,7 @@ def play_game(board_size: int, num_players: int) -> int:
                 break
             row += 1
         # Validate whether the current player has won
-        if validate_win(cur_player, board, board_size, row, selected_col):
+        if validate_win(board, board_size, row, selected_col, cur_player):
             print(*board, sep='\n')
             return cur_player
         # Validate whether the game can continue (if all are non-zero in the top row, the game ends in a tie)
